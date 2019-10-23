4a4a6731eb2013cd92028b100b0271ba5f8d26f1
Raphael Rappoli <rapharappoli@hotmail.com>
2019-10-23 17:57:38 -0300

    creating login page and routes

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,34 +1,37 @@
-import React from "react";
-import navMenuStyles from "./navMenu.module.css";
+import React from "react"
+
+import { Link } from 'react-router-dom'
+
+import navMenuStyles from "./navMenu.module.css"
 
 function NavMenu(props) {
-  return (
-    <nav className={navMenuStyles.navMenu}>
-      <ul className={navMenuStyles.navMenu__lista}>
-        <li className={navMenuStyles.navMenu__item}>
-          <a className={navMenuStyles.navMenu__link} href="/">
-            Bem vindo(a): <br />
-            <strong>{props.usuario}</strong>
-          </a>
-        </li>
-        <li className={navMenuStyles.navMenu__item}>
-          <a className={navMenuStyles.navMenu__link} href="/">
-            Página Inicial
-          </a>
-        </li>
-        <li className={navMenuStyles.navMenu__item}>
-          <a className={navMenuStyles.navMenu__link} href="/hashtags">
-            Hashtags
-          </a>
-        </li>
-        <li className={navMenuStyles.navMenu__item}>
-          <a className={navMenuStyles.navMenu__link} href="/logout">
-            Logout
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className={navMenuStyles.navMenu}>
+			<ul className={navMenuStyles.navMenu__lista}>
+				<li className={navMenuStyles.navMenu__item}>
+					<a className={navMenuStyles.navMenu__link} href="/">
+						Bem vindo(a): <br />
+						<strong>{props.usuario}</strong>
+					</a>
+				</li>
+				<li className={navMenuStyles.navMenu__item}>
+					<Link className={navMenuStyles.navMenu__link} to="/login">
+						Página Inicial
+          			</Link>
+				</li>
+				<li className={navMenuStyles.navMenu__item}>
+					<a className={navMenuStyles.navMenu__link} href="/hashtags">
+						Hashtags
+         			</a>
+				</li>
+				<li className={navMenuStyles.navMenu__item}>
+					<a className={navMenuStyles.navMenu__link} href="/logout">
+						Logout
+          			</a>
+				</li>
+			</ul>
+		</nav>
+	)
 }
 
 export { NavMenu }
